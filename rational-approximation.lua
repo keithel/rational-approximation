9505670099fcac85f694a6af65642b4956af08b8
Keith Kyzivat <kamaji@gmail.com>
2014-10-23 11:29:26 -0400

    Separate out the command line runnability from the function

--- rational-approximation.lua
+++ rational-approximation.lua
@@ -5,7 +5,7 @@
 -- With corrections from Arno Formella, May 2008
 -- Translation to Lua by Keith Kyzivat, Oct 2014
 --
--- usage: lua rational-approximation.lua r d
+-- usage: lua rational-approximation-cmdline.lua r d
 --   r is real number to approx
 --   d is the maximum denominator allowed
 --
@@ -72,13 +72,3 @@ function toFraction(startx, maxdenominator)
     return result
 end
 
--- read command line arguments
-if (#arg ~= 2) then
-    io.stderr:write("usage: lua fractions.lua r d\n") -- AF: argument missing
-    os.exit(1)
-end
-
-local fracs = toFraction(tonumber(arg[1]), math.floor(tonumber(arg[2])))
-io.write("Results:\n")
-io.write(string.format("  (1) %5d/%-5d    error %13e\n", fracs[1][1], fracs[1][2], fracs[1][3]))
-io.write(string.format("  (2) %5d/%-5d    error %13e\n", fracs[2][1], fracs[2][2], fracs[2][3]))
